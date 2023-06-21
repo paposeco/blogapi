7e76c712fd58d9e0b8f84ad98d60af9e9d74763d
paposeco <13892562+paposeco@users.noreply.github.com>
2023-06-21 11:02:43 +0100

    added railway port env

--- index.js
+++ index.js
@@ -57,3 +57,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+// Use PORT provided in environment or default to 3000
+const port = process.env.PORT || 3000;
+
+// Listen on `port` and 0.0.0.0
+app.listen(port, "0.0.0.0", function() {
+  // ...
+});
